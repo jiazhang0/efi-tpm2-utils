d07bd99db9336a22bb63690c6687482bf36d06c6
Lans Zhang <jia.zhang@windriver.com>
2016-12-15 21:54:45 +0800

    tcg2: fix the failure of calling GetCapability() if Size is specified
    Signed-off-by: Lans Zhang <jia.zhang@windriver.com>

--- src/efi/tcg2.c
+++ src/efi/tcg2.c
@@ -33,7 +33,7 @@ Tcg2LocateProtocol(EFI_TCG2_PROTOCOL **Tcg2)
 
 EFI_STATUS
 Tcg2GetCapability(EFI_TCG2_BOOT_SERVICE_CAPABILITY *Capability,
-		  UINT8 *Size)
+		  UINT8 *CapabilitySize)
 {
 	EFI_TCG2_PROTOCOL *Tcg2;
 	EFI_STATUS Status;
@@ -46,53 +46,24 @@ Tcg2GetCapability(EFI_TCG2_BOOT_SERVICE_CAPABILITY *Capability,
 
 	EFI_TCG2_BOOT_SERVICE_CAPABILITY TpmCapability;
 
-	if (!*Size) {
+	if (!*CapabilitySize) {
 		TpmCapability.Size = (UINT8)sizeof(TpmCapability);
 		Capability = &TpmCapability;
-	}
+	} else
+		Capability->Size = *CapabilitySize;
 
 	Status = uefi_call_wrapper(Tcg2->GetCapability, 2, Tcg2,
 				   Capability);
 	if (EFI_ERROR(Status)) {
 		Print(L"Unable to get the TPM capability: %r\n", Status);
+		if (Status == EFI_BUFFER_TOO_SMALL)
+			Print(L"Only %d-byte is filled in the capability "
+			      L"structure\n", TpmCapability.Size);
 		return Status;
 	}
 
-	if (!*Size)
-		*Size = TpmCapability.Size;
+	if (!*CapabilitySize)
+		*CapabilitySize = Capability->Size;
 
 	return Status;
-}
-
-EFI_STATUS
-Tcg2DetectTPM(VOID)
-{
-	EFI_TCG2_BOOT_SERVICE_CAPABILITY TpmCapability;
-	UINTN TpmCapabilitySize = sizeof(TpmCapability);
-	EFI_STATUS Status;
-
-	Status = Tcg2GetCapability(&TpmCapability,
-				   (UINT8 *)&TpmCapabilitySize);
-	if (EFI_ERROR(Status))
-		return Status;
-
-	if (TpmCapability.StructureVersion.Major == 1 &&
-			TpmCapability.StructureVersion.Minor == 0) {
-		EFI_TCG_BOOT_SERVICE_CAPABILITY *Tpm1Capability;
-
-		Tpm1Capability = (EFI_TCG_BOOT_SERVICE_CAPABILITY *)&TpmCapability;
-		if (Tpm1Capability->TPMPresentFlag) {
-			Print(L"TPM 1.x device detected\n");
-			return EFI_SUCCESS;
-		}
-	} else {
-		if (TpmCapability.TPMPresentFlag) {
-			Print(L"TPM 2.0 device detected\n");
-			return EFI_SUCCESS;
-		}
-	}
-
-	Print(L"No TPM device detected\n");
-
-	return EFI_NOT_FOUND;
 }
